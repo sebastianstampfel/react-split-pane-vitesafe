6b68a84a45cbe8b914a74ac563c810cef934fb20
Sebastian Stampfel <s.stampfel@planradar.com>
2023-06-02 14:29:16 +0200

    Remove styled components lib

--- src/Resizer.js
+++ src/Resizer.js
@@ -1,74 +1,38 @@
-import React, { Component } from 'react';
-import styled from 'styled-components';
+import React, { Component } from "react";
+import "./resizer.css";
 
-const Wrapper = styled.div`
-  background: #000;
-  opacity: 0.2;
-  z-index: 1;
-  box-sizing: border-box;
-  background-clip: padding-box;
+const VerticalWrapper = ({ children, ...props }) => {
+  return (
+    <div className="wrapper vertical" {...props}>
+      {children}
+    </div>
+  );
+};
 
-  :hover {
-    transition: all 2s ease;
-  }
-`;
-
-const HorizontalWrapper = styled(Wrapper)`
-  height: 11px;
-  margin: -5px 0;
-  border-top: 5px solid rgba(255, 255, 255, 0);
-  border-bottom: 5px solid rgba(255, 255, 255, 0);
-  cursor: row-resize;
-  width: 100%;
-
-  :hover {
-    border-top: 5px solid rgba(0, 0, 0, 0.5);
-    border-bottom: 5px solid rgba(0, 0, 0, 0.5);
-  }
-
-  .disabled {
-    cursor: not-allowed;
-  }
-  .disabled:hover {
-    border-color: transparent;
-  }
-`;
-
-const VerticalWrapper = styled(Wrapper)`
-  width: 11px;
-  margin: 0 -5px;
-  border-left: 5px solid rgba(255, 255, 255, 0);
-  border-right: 5px solid rgba(255, 255, 255, 0);
-  cursor: col-resize;
-
-  :hover {
-    border-left: 5px solid rgba(0, 0, 0, 0.5);
-    border-right: 5px solid rgba(0, 0, 0, 0.5);
-  }
-  .disabled {
-    cursor: not-allowed;
-  }
-  .disabled:hover {
-    border-color: transparent;
-  }
-`;
+const HorizontalWrapper = ({ children, ...props }) => {
+  return (
+    <div className="wrapper horizontal" {...props}>
+      {children}
+    </div>
+  );
+};
 
 class Resizer extends Component {
   render() {
     const {
       index,
-      split = 'vertical',
+      split = "vertical",
       onClick = () => {},
       onDoubleClick = () => {},
       onMouseDown = () => {},
       onTouchEnd = () => {},
-      onTouchStart = () => {},
+      onTouchStart = () => {}
     } = this.props;
 
     const props = {
       ref: _ => (this.resizer = _),
-      'data-attribute': split,
-      'data-type': 'Resizer',
+      "data-attribute": split,
+      "data-type": "Resizer",
       onMouseDown: event => onMouseDown(event, index),
       onTouchStart: event => {
         event.preventDefault();
@@ -89,10 +53,10 @@ class Resizer extends Component {
           event.preventDefault();
           onDoubleClick(event, index);
         }
-      },
+      }
     };
 
-    return split === 'vertical' ? (
+    return split === "vertical" ? (
       <VerticalWrapper {...props} />
     ) : (
       <HorizontalWrapper {...props} />
